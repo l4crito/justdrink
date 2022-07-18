460bc5e8ec0dc2899a276a4e0fb4f89a52b8dc9c
Christian Caicedo <christian.caicedo@grainger.com>
2022-07-18 14:26:07 -0500

    filtro por jugador

--- src/app/drink/components/history/history.component.ts
+++ src/app/drink/components/history/history.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HistoryModel } from 'src/app/models/history.model';
+import { PlayerModel } from 'src/app/models/player.model';
+import { PlayerTaskModel } from 'src/app/models/task.model';
 import { TaskProvider } from 'src/app/shared/provider/task.provider';
 
 @Component({
@@ -9,8 +11,23 @@ import { TaskProvider } from 'src/app/shared/provider/task.provider';
 })
 export class HistoryComponent implements OnInit {
   history: HistoryModel[] = [];
+  title = "Historial"
+  selectedPlayer: PlayerModel | null = null;
   constructor(public taskProvider: TaskProvider) {
-    taskProvider.history?.slice().splice(1).forEach(task => {
+    this.initHistory();
+  }
+
+  ngOnInit(): void {
+  }
+  initHistory() {
+    this.title="Historial"
+    this.history = [];
+    this.selectedPlayer = null;
+    this.getHisthory(this.taskProvider.history?.slice().splice(1));
+  }
+
+  getHisthory(h: PlayerTaskModel[]) {
+    h.forEach(task => {
       const line = this.history.find(t => t.round === task.round)
       if (line) {
         line.tasks.push(task)
@@ -18,10 +35,16 @@ export class HistoryComponent implements OnInit {
         this.history.push({ round: task.round, tasks: [task] })
       }
     });
-    console.log(this.history)
   }
 
-  ngOnInit(): void {
+  filterHistory(player?: PlayerModel | undefined | null) {
+    if (!player) {
+      return;
+    }
+    this.history = [];
+    this.title = "Historial de ";
+    this.selectedPlayer = player;
+    this.getHisthory(this.taskProvider.history?.filter(h => h.player?.name === player.name));
   }
 
 }
